4bd678ff2a5ebbd31bdf1c3b51aa93ccb5eee99c
meruem243 <mughees.ahmed243@gmail.com>
2023-07-23 23:43:13 +0500

    Created Stateless Widget Showing AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text(
+          'Ask Me Anything',
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
 }
